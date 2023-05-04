215221de79a0688b81417668cc8c02277141dd93
Anusha Somaraddi <asomaraddi@BLR.ALLEGISINDIA.COM>
2023-05-05 00:09:05 +0530

    javaaws

--- SpringBootSampleApp/src/main/java/com/example/SpringBootSampleApp/SpringBootSampleAppApplication.java
+++ SpringBootSampleApp/src/main/java/com/example/SpringBootSampleApp/SpringBootSampleAppApplication.java
@@ -8,7 +8,7 @@ public class SpringBootSampleAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleAppApplication.class, args);
-		System.out.println("hello");
+		
 	}
 
 }
